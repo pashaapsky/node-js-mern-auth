5b3070c4406aa8e15e185749eb84ac50e01cd99a
pashaapsky <ap.sky@yandex.ru>
2020-11-30 22:40:21 +0300

    working with axios requests

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,47 @@
-import React, {useEffect, useState, Fragment} from 'react';
+import React, {useEffect, useState, Fragment, useCallback} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import axios from './axios'
+import axios from 'axios'
 
 function App() {
     const [user, setUser] = useState(null);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    useEffect(() => {
-
-    });
-
     const register = async (event) => {
         event.preventDefault();
 
         try {
-            // const response = await axios.post('/api/auth/register', {email, password});
-            const response = await axios.post('/api/auth/register', {email: 'test@mail.ru', password: '123123'});
-            // const response = await axios.post('/api/auth/test');
+            const response = await axios.post('/api/auth/register', {email, password})
+                .then(res => res.data)
+                .catch(error => {
+                    // обработка ошибок
+                    throw new Error(error.response.data.message);
+                })
+            ;
 
-            console.log(response);
-
-        } catch (e) {
-            console.log('Ошибка: ', e);
+            setUser(email);
+        }
+        catch (e) {
+            alert(e.message)
         }
     };
 
     const login = async () => {
+        try {
+            const response = await axios.post('/api/auth/login', {email, password})
+                .then(res => res.data)
+                .catch(error => {
+                    // обработка ошибок
+                    throw new Error(error.response.data.message);
+                })
+            ;
 
+            setUser(email);
+        }
+        catch (e) {
+            alert(e.message)
+        }
     };
-
-    // console.log('email: ', email);
-    // console.log('password: ', password);
 
     return (
         <div className="App">
@@ -51,10 +61,12 @@ function App() {
 
                             <form action="" onSubmit={login}>
                                 <label htmlFor="login-email">Введите email</label>
-                                <input id="login-email" type="email" name="email" value={email} onChange={event => setEmail(event.target.value)}/>
+                                <input id="login-email" type="email" name="email" value={email}
+                                       onChange={event => setEmail(event.target.value)}/>
 
                                 <label htmlFor="login-password">Введите пароль</label>
-                                <input id="login-password" type="password" name="password" value={password} onChange={event => setPassword(event.target.value)}/>
+                                <input id="login-password" type="password" name="password" value={password}
+                                       onChange={event => setPassword(event.target.value)}/>
 
                                 <button type="submit">Log In</button>
                             </form>
@@ -65,10 +77,12 @@ function App() {
 
                             <form action="" onSubmit={register}>
                                 <label htmlFor="reg-email">Введите email</label>
-                                <input id="reg-email" type="email" name="email" value={email} onChange={event => setEmail(event.target.value)}/>
+                                <input id="reg-email" type="email" name="email" value={email}
+                                       onChange={event => setEmail(event.target.value)}/>
 
                                 <label htmlFor="reg-password">Введите пароль</label>
-                                <input id="reg-password" type="password" name="password" value={password} onChange={event => setPassword(event.target.value)}/>
+                                <input id="reg-password" type="password" name="password" value={password}
+                                       onChange={event => setPassword(event.target.value)}/>
 
                                 <button type="submit">Register</button>
                             </form>
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -18,7 +18,7 @@ router.post(
             const errors = validationResult(req);
 
             if (!errors.isEmpty()) {
-                return res.status(400).json({
+                return res.status(400).send({
                     errors: errors.array(),
                     message: "Некорректные данные при регистрации"
                 })
@@ -29,19 +29,18 @@ router.post(
             const candidate = await User.findOne({email: email});
 
             if (candidate) {
-                return res.status(400).json({message: 'Такой пользователь уже существует'})
+                return res.status(400).send({message: 'Такой пользователь уже существует'})
             }
-            //
-            // const hashedPassword = await bcrypt.hash(password, 12);
-            // const user = new User({email: email, password: hashedPassword});
-            //
-            // await user.save();
-            //
-            res.status(201).json({message: 'Пользователь создан'});
 
-            // res.status(200).json({message: 'HELLO BROther'});
+            const hashedPassword = await bcrypt.hash(password, 12);
+            const user = new User({email: email, password: hashedPassword});
+
+            await user.save();
+
+            res.status(201).send({message: 'Пользователь создан'});
+
         } catch (e) {
-            res.status(500).json({message: "Что то пошло не так, попробуйте снова"})
+            res.status(500).send({message: "Что то пошло не так, попробуйте снова"})
         }
     });
 
@@ -102,7 +101,7 @@ router.get('/test', async (req, res) => {
 
 router.post('/test', async (req, res) => {
     try {
-        res.status(200).send('Hello bro from post');
+        res.status(200).send({message: 'Hello bro from post'});
     } catch (e) {
 
     }
